7ce1bc2063d3f0a62e38c737e5c12136bad746a9
Jeff Stringer & Sergii Mertsalov <student@epicodus.com>
2014-08-11 15:08:42 -0700

    add projects belongs to employees

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe Project do
-  it 'has one employee' do
-
+  it 'belongs to an employee' do
+    division = Division.create({:name => "Accounting"})
+    employee = Employee.create({:name => "Ricky", :division_id => division.id})
+    project = Project.create({:name => "Clean Gutters", :employee_id => employee.id})
+    expect(project.employee_id).to eq employee.id
   end
 end
